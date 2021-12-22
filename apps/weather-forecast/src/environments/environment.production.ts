207d6bea324bb69cdb020cadd3f419d7a18852f2
Christos Krezias <ckrezias3@gatech.edu>
2021-12-22 10:46:04 +0200

    Add ngrx and configure debug environment

--- apps/weather-forecast/src/environments/environment.production.ts
+++ apps/weather-forecast/src/environments/environment.production.ts
@@ -1,3 +1,4 @@
 export const environment = {
 	production: true,
+	openweathermapApiKey: '9560ad36dc2e8f67fe9006d94f3009b0',
 };
--- apps/weather-forecast/src/environments/environment.ts
+++ apps/weather-forecast/src/environments/environment.ts
@@ -6,6 +6,7 @@
 
 export const environment = {
 	production: false,
+	openweathermapApiKey: '9560ad36dc2e8f67fe9006d94f3009b0',
 };
 
 /*
